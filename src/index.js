0fd19a7bf94607fc02136f25d2bcdd86b706eb4a
Jason Hang <jason.hang@fareportal.com>
2018-07-02 23:19:43 -0400

    added sum to the index file.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Nested from './Nested';
 import SelectInput from './SelectInput';
 import TextInput from './TextInput';
 import Widget from './Widget';
+import sum from './sum';
 
 export {
   Calendar,
   Nested,
   SelectInput,
   TextInput,
-  Widget
+  Widget,
+  sum
 };
