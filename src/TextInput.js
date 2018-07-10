29ce15ad96056d60a856c6588fe0b268d5599f00
Jason Hang <jason.hang@fareportal.com>
2018-07-09 23:17:51 -0400

    Updated TexTInput.

--- src/TextInput.js
+++ src/TextInput.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextInput = ({ type, value, onChange }) => (
-  <input type={type} value={value} onChange={onChange} />
+const TextInput = ({ type, value, onChange, placeholder }) => (
+  <input type={type} value={value} onChange={onChange} placeholder={placeholder} />
 );
 
 TextInput.propTypes = {
   type: PropTypes.string,
   value: PropTypes.string,
+  placeholder: PropTypes.string<
   onChange: PropTypes.func
 };
 
