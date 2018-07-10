69bb217db61bf7ca603cc64e99ad3bddb98b3cc6
Jason Hang <jason.hang@fareportal.com>
2018-07-09 23:19:43 -0400

    Updated textinput.

--- src/TextInput.js
+++ src/TextInput.js
@@ -8,7 +8,7 @@ const TextInput = ({ type, value, onChange, placeholder }) => (
 TextInput.propTypes = {
   type: PropTypes.string,
   value: PropTypes.string,
-  placeholder: PropTypes.string<
+  placeholder: PropTypes.string,
   onChange: PropTypes.func
 };
 
